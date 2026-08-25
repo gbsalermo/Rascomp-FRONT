c9ca9e8715f94314c3255861c6672cd605513a60
Salermo <119875310+gbsalermo@users.noreply.github.com>
2026-08-24 22:24:28 -0300

    style: carrega estilos dedicados do login

--- gestao/src/main.ts
+++ gestao/src/main.ts
@@ -4,6 +4,7 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './styles/main.css'
 import './styles/management-ui.css'
+import './styles/login-ui.css'
 import App from './App.vue'
 import router from './router'
 
